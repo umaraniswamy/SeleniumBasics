d5c594af15377b8d2ec75f2f51dd353e202fe789
umarani <umatesting15@gmail.com>
2022-03-17 12:49:38 +0000

    made changes in develop branch

--- src/test/java/alerts/AlertsExp.java
+++ src/test/java/alerts/AlertsExp.java
@@ -10,35 +10,36 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AlertsExp {
     public static void main(String[] args) {
+        WebDriverWait wait;
+        String result;
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("http://the-internet.herokuapp.com/");
         driver.findElement(By.linkText("JavaScript Alerts")).click();
 
-       /* alert1
+
        driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
-        WebDriverWait wait=new WebDriverWait(driver,20);
+         wait=new WebDriverWait(driver,20);
         wait.until(ExpectedConditions.alertIsPresent());
         driver.switchTo().alert().accept();
-       String result= driver.findElement(By.id("result")).getText();
-        System.out.println("result is..."+result);*/
+        result= driver.findElement(By.id("result")).getText();
+        System.out.println("result is..."+result);
 
-       /* alert 2
         driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
-        WebDriverWait wait=new WebDriverWait(driver,20);
+         wait=new WebDriverWait(driver,20);
         wait.until(ExpectedConditions.alertIsPresent());
        String alertText= driver.switchTo().alert().getText();
         System.out.println("text in the alert"+alertText);
         driver.switchTo().alert().dismiss();
-        String result=driver.findElement(By.id("result")).getText();
-        System.out.println("the result is.."+result);*/
+         result=driver.findElement(By.id("result")).getText();
+        System.out.println("the result is.."+result);
 
         driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
-        WebDriverWait wait=new WebDriverWait(driver,20);
+         wait=new WebDriverWait(driver,20);
         wait.until(ExpectedConditions.alertIsPresent());
         driver.switchTo().alert().sendKeys("hello world");
         driver.switchTo().alert().accept();
-        String result=driver.findElement(By.id("result")).getText();
+         result=driver.findElement(By.id("result")).getText();
         System.out.println("the result is.."+result);
 
 
